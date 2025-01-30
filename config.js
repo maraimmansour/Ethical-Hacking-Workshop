0aaa4295b74b995467a1bef35479176726662561
maraimmansour <143694057+maraimmansour@users.noreply.github.com>
2025-01-30 23:06:01 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration for the application
+const appConfig = {
+    appName: "FakeBank Financial",
+    version: "1.0",
+    apiBaseUrl: "https://api.fakebank.com"
+  };
+  
+  console.log(`Loaded ${appConfig.appName} configuration, version ${appConfig.version}`);
+  
