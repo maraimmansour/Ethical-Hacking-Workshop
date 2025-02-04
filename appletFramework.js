ccec1aa008e5e77203a1816213c3d4fe159261cf
Mariam mansour <143694057+maraimmansour@users.noreply.github.com>
2025-02-04 16:44:52 +0400

    Delete appletFramework.js

--- appletFramework.js
+++
@@ -1,7 +0,0 @@
-// Placeholder framework for future applet-related functionalities.
-console.log("Applet framework loaded");
-
-// Example functionality
-function logActivity(activity) {
-  console.log(`User activity: ${activity}`);
-}
