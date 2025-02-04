e225f844f973de2448474ecea27ac82bded32b5d
Mariam mansour <143694057+maraimmansour@users.noreply.github.com>
2025-02-04 16:45:07 +0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Configuration for the application
-const appConfig = {
-    appName: "FakeBank Financial",
-    version: "1.0",
-    apiBaseUrl: "https://api.fakebank.com"
-  };
-  
-  console.log(`Loaded ${appConfig.appName} configuration, version ${appConfig.version}`);
-  
